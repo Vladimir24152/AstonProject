c2f32f88aee233ceddb01291bdacd3fb38fba054
Malyshev Vladimir <mvs123331@gmail.com>
2025-12-12 17:17:45 +0400

    [feature] добавил реализацию коллекции

--- src/main/java/org/example/collection/StudentArrayList.java
+++ src/main/java/org/example/collection/StudentArrayList.java
@@ -4,22 +4,33 @@ import org.example.model.Student;
 
 public class StudentArrayList implements StudentList{
 
-    Student[] students = new Student[10];
+    private Integer CAPACITY = 10;
 
-    Integer size;
+    private Student[] students = new Student[CAPACITY];
+
+    private Integer size;
 
     @Override
     public void add(Student student) {
-
+        if (size == students.length){
+            CAPACITY = CAPACITY * 2;
+            Student[] newStudents = new Student[CAPACITY];
+            for (int i = 0; i < students.length; i++){
+                newStudents[i] = students[i];
+            }
+            students = newStudents;
+        }
+        students[size] = student;
+        size++;
     }
 
     @Override
     public void clean() {
-
+        students = new Student[10];
     }
 
     @Override
     public Student get(Integer index) {
-        return null;
+        return students[index];
     }
 }
---
+++ src/test/java/org/example/collection/StudentArrayListTest.java
@@ -0,0 +1,13 @@
+package org.example.collection;
+
+
+import org.example.model.Student;
+import org.junit.jupiter.api.Test;
+
+class StudentArrayListTest {
+
+    @Test
+    public void whenAdd3StudentSizeIs3(){
+
+    }
+}
