f305631757dc1e392dd79e4d6645af0a1b78f784
sushandrew <asushkov2023@vk.com>
2025-12-14 19:56:06 +0300

    Реализация вывода студентов в консоль

--- src/main/java/org/example/io/printer/impl/PrintStudentsToConsole.java
+++ src/main/java/org/example/io/printer/impl/PrintStudentsToConsole.java
@@ -6,6 +6,8 @@ import org.example.io.printer.PrintStudents;
 public class PrintStudentsToConsole implements PrintStudents {
     @Override
     public void printStudents(StudentList studentList) {
-
+        for (int i = 0; i < studentList.size(); i++) {
+            System.out.println(studentList.get(i).toString());
+        }
     }
 }
