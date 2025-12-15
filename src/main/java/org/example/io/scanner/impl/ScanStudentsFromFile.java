658f3fc42153d920b315cbb817aea49bc698eb9c
Vladislav Elizarov <velizarovdev@gmail.com>
2025-12-15 20:31:29 +0300

    Удалил ненужный комментарий

--- src/main/java/org/example/io/scanner/impl/ScanStudentsFromFile.java
+++ src/main/java/org/example/io/scanner/impl/ScanStudentsFromFile.java
@@ -33,33 +33,6 @@ public class ScanStudentsFromFile implements ScanStudents {
                 .limit(count)
                 .collect(new StudentArrayListCollector());
 
-//        for (int i = 0; i < count; i++) {
-//            if (i == students.size()) {
-//                break;
-//            }
-//            String[] element = students.get(i).split("\\|");
-//
-//            String name = element[0];
-//            String lastname = element[1];
-//            Integer groupNumber = Integer.valueOf(element[2]);
-//            Double averageScore = Double.valueOf(element[3]);
-//            String gradeBookNumber = element[4];
-//            Integer age = Integer.valueOf(element[5]);
-//            String address = element[6];
-//
-//            // Создаю объект Student
-//            Student student;
-//            try {
-//                student = new Student.Builder(name, lastname, groupNumber, averageScore, gradeBookNumber)
-//                        .buildAge(age)
-//                        .buildAddress(address)
-//                        .build();
-//            } catch (IllegalStudentException e) {
-//                throw new RuntimeException(e);
-//            }
-//            // Добавить студента в коллекцию
-//            result.add(student);
-//        }
         return result;
     }
 }
