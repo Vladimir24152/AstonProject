d10f9e28317850ccb5dd220ed2f6a9ce8a44e62a
iankudinov <pragliding86@gmail.com>
2025-12-16 21:59:04 +0300

    Уточнил типы переменных и их названия в Student

--- src/main/java/org/example/io/scanner/impl/ScanStudentsRandom.java
+++ src/main/java/org/example/io/scanner/impl/ScanStudentsRandom.java
@@ -7,30 +7,38 @@ import java.util.Random;
 
 public class ScanStudentsRandom implements ScanStudents {
 
-    private static final String[] Names = new String[]{"Alexey", "Ivan", "Olga", "Evgeny", "Yulia", "Nikolay", "Yury", "Anna", "Anton", "Dmitry"};
-    private static final String[] LastNames = new String[]{"Vorobyev", "Smirnov", "Sokolova", "Tumanov", "Alexeeva", "Petrov", "Frolov", "Kozlova", "Koshkin", "Dmitriev"};
-    private static final Integer[] GroupNumber = new Integer[]{101, 105, 108, 130, 211, 213, 220, 226, 338, 340, 360, 361, 477, 478, 423, 442, 502, 506, 507, 510};
-    private static final Double[] AverageScore = new Double[]{2.0, 2.3, 2.5, 2.8, 3.0, 3.3, 3.5, 3.8, 4.0, 4.3, 4.5, 4.8, 5.0};
-    private static final Integer[] GradeBookNumber = new Integer[]{3334445, 7778844, 6645568, 9596949, 4477885, 2123454, 3121342, 5557556, 9959461, 7679481};
-    private static final String[] Address = new String[]{"Moscow, Paveletskaya 48, 5", "Moskow, Smolenskaya 11, 13 ", "Moskow, Petrovskaya 20, 4", "Moskow, Liteynaya 16, 109"};
-    private static final Integer[] Age = new Integer[]{16, 17, 18, 19, 20, 21, 22, 23};
+    private static final String[] names = new String[]{"Alexey", "Ivan", "Olga", "Evgeny", "Yulia", "Nikolay", "Yury", "Anna", "Anton", "Dmitry"};
+    private static final String[] lastNames = new String[]{"Vorobyev", "Smirnov", "Sokolova", "Tumanov", "Alexeeva", "Petrov", "Frolov", "Kozlova", "Koshkin", "Dmitriev"};
+    private static final String[] city = new String[]{"Moscow, ", "Saratov, ", "Perm, ", "Tumen, ", "Kursk, ", "Ekaterinburg, ", "Tver, ", "Volgograd, ", "Samara, ", "Tambov, "};
+    private static final String[] street = new String[]{"Paveletskaya, ", "Smolenskaya, ", "Petrovskaya, ", "Moskowskaya, ", "Orekhovskaya, ", "Zelenaya, ", "Yagodnaya, ", "Futbolnaya, "};
 
     @Override
     public StudentList scanStudents(Integer count) {
-        StudentList list = new StudentArrayList();
-        Random random = new Random();
-        for (int i = 0; i < count; i++ ){
-            Student student = new Student.Builder(
-                    Names[random.nextInt(Names.length -1)],
-                    LastNames[random.nextInt(LastNames.length -1)],
-                    GroupNumber[random.nextInt(GroupNumber.length -1)],
-                    AverageScore[random.nextInt(AverageScore.length -1)],
-                    GradeBookNumber[random.nextInt(GradeBookNumber.length -1)])
-                    .buildAge(1) //Age[random.nextInt(Age.length -1)]) - изменяет конструктор в Builder класса Student
-                    .buildAddress("")//Address[random.nextInt(Address.length -1)] - аналогично
-                    .build();
-            list.add(student);
+        StudentList list;
+        Random random;
+        try {
+            random = new Random();
+            list = new StudentArrayList();
+            for (int i = 0; i < count; i++ ){
+            new StudentArrayList();
+            Student student;
+                student = new Student.Builder(
+                        names[random.nextInt(names.length -1)], //имена
+                        lastNames[random.nextInt(lastNames.length -1)], //фамилии
+                        random.nextInt(100, 600), //номера групп
+                        Math.round((2.0 + 3.0 * random.nextDouble()) * 100.0) / 100.0,//средний балл с округлением до 2 знаков после запятой
+                        String.valueOf(random.nextInt(1_000_000, 10_000_000)),//номер зачетки
+                        .buildAge(random.nextInt(16, 100 +1)) //возраст
+                        .buildAddress(city[random.nextInt(city.length -1)] //города
+                                + street[random.nextInt(street.length -1)] //улицы
+                                + random.nextInt(200) + ", " //номер дома
+                                + random.nextInt(500) + ";") //номер квартиры
+                        .build();
+                list.add(student);
         }
         return list;
+        } catch (IllegalStudentException e) {
+          throw new RuntimeException(e);
+        }
     }
 }
